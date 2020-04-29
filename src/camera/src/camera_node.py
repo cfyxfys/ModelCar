8c3767b3dba1549ca355c17f322e06ffb533b904
mate <cfyxfys@gmail.com>
2020-04-29 14:28:47 +0200

    VideoRaw Topic is published

--- src/camera/src/camera_node.py
+++ src/camera/src/camera_node.py
@@ -2,6 +2,9 @@
 
 import rospy
 import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 def camera():
 	rospy.init_node('camera', anonymous = True)
 	rate = rospy.Rate(30)
@@ -10,16 +13,19 @@ def camera():
 	if not vcap.isOpened():
 		return "can not open stream"
 
+	videoRaw = rospy.Publisher('VideoRaw', Image, queue_size = 10)
 		
 	while vcap.isOpened():
+
 		ret, frame = vcap.read()
 		cv2.imshow('VIDEO', frame)
+		msg_frame = CvBridge().cv2_to_imgmsg(frame)
+		videoRaw.publish(msg_frame)
+		cv2.waitKey(1)
 
-		# break if pressed any key
-		if cv2.waitKey(1) != -1:
-			break
 	vcap.release()
 	cv2.destroyAllWindows()
+
 
 
 
